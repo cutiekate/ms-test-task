63ba979b0af9ebac9877c095e3c5ae02a380a57b
kate <kate@test.com>
2020-09-25 15:26:36 +0300

    carousel added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,31 @@
-'use strict';
+/* eslint-disable strict */
+
+// eslint-disable-next-line strict
+// eslint-disable-next-line no-undef
+// eslint-disable-next-line strict
+// eslint-disable-next-line no-undef
+// eslint-disable-next-line strict
+// eslint-disable-next-line no-undef
+$(document).ready(function() {
+  // eslint-disable-next-line no-undef
+  $('.owl-carousel').owlCarousel();
+});
+
+// eslint-disable-next-line no-undef
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
