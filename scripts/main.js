2097496726e9b3c46b3f15d7f235f8db815f3923
kate <kate@test.com>
2020-09-28 10:02:20 +0300

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 $('.owl-carousel').owlCarousel({
   loop: true,
   margin: 10,
-  nav: true,
+  nav: false,
   autoplay: true,
   responsive: {
     0: {
@@ -29,3 +29,57 @@ $('.owl-carousel').owlCarousel({
     },
   },
 });
+
+function countdown(timeLeft) {
+  let currentTime = timeLeft;
+
+  setInterval(calculate, 1000);
+
+  function calculate() {
+    if (currentTime-- >= 0) {
+      const days = parseInt((currentTime / 86400));
+      const hours = parseInt((currentTime / 3600) % 24);
+      const minutes = parseInt((currentTime / 60) % 60);
+      const seconds = parseInt(currentTime % 60);
+
+      document.getElementById('days').innerHTML
+        = days.toString().padStart(2, '0');
+
+      document.getElementById('hours').innerHTML
+        = hours.toString().padStart(2, '0');
+
+      document.getElementById('minutes').innerHTML
+        = minutes.toString().padStart(2, '0');
+
+      document.getElementById('seconds').innerHTML
+        = seconds.toString().padStart(2, '0');
+    } else {
+
+    }
+  }
+
+  // function display(days, hours, minutes, seconds) {}
+}
+
+countdown(660);
+
+function onClick(e) {
+
+  const m = new Map(
+    ['01', {
+      imgSrc: '',
+      text: 'lol',
+    }],
+    ['02', {
+      imgSrc: '',
+      text: 'lol',
+    }],
+    ['03', {
+      imgSrc: '',
+      text: 'lol',
+    }],
+  );
+
+  const data = m[e.target.innerHTML];
+  // find DOM
+}
